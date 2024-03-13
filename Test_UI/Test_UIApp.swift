5c7af21aa0b0797e9c3c1e91f922cd65708588fb
Alex_Kogut13 <akogut@engenious.io>
2024-03-13 13:33:39 +0200

    Initial Commit

---
+++ Test_UI/Test_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_UIApp.swift
+//  Test_UI
+//
+//  Created by Alex Kogut on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
